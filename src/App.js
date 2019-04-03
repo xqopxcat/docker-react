576442610e9d76aeb665e574bfd57ee8abc8c13b
Steve Lee <steve.lee@nexusguard.com>
2019-04-04 01:28:23 +0800

    changed feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fuck you All
+            I was changed on the feature branch
           </p>
           <a
             className="App-link"
